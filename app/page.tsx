869e4c81d8bbc462f9088c8ffe2dc55a6816a6a6
Omsurvase24 <omsurvase24@gmail.com>
2024-08-18 02:41:32 +0530

    setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <p className="text-red-500 font-bold">
+      <Button variant="custom">
+        Click Me
+      </Button>
+    </p>
+  );
+}
