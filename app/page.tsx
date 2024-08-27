4f6aa217290d0f0be825106fde599a51426be8fe
Omsurvase24 <omsurvase24@gmail.com>
2024-08-27 15:23:01 +0530

    auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
+// app/page.tsx
+"use client";
+import { UserButton } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useAuth } from "@clerk/nextjs";
+
 export default function Page() {
+  const { isSignedIn } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isSignedIn) {
+      router.push("/sign-in");
+    }
+  }, [isSignedIn, router]);
+
   return (
-    <p>
-      Only auth users can see this
-    </p>
+    <div>
+      <h1>Welcome to the Home Page!</h1>
+      <UserButton />
+
+    </div>
   );
 }
