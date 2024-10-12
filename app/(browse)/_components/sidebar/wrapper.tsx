32e86917fc3f0dae59ed25bd74eca8c07759fb41
Omsurvase24 <omsurvase24@gmail.com>
2024-10-12 16:05:35 +0530

    useIsClient

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useIsClient } from "usehooks-ts";
 import { cn } from "@/lib/utils";
 import { useSidebar } from "@/store/use-sidebar";
 import { ToggleSkeleton } from "./toggle";
@@ -13,12 +13,8 @@ interface WrapperProps {
 export const Wrapper = ({
     children,
 }: WrapperProps) => {
-    const [isClient, setIsClient] = useState(false);
+    const isClient = useIsClient();
     const { collapsed } = useSidebar((state) => state);
-
-    useEffect(() => {
-        setIsClient(true);
-    }, []);
 
     if (!isClient) return (
         <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50">
