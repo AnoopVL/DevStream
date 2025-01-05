9ab84b151c845dac84d8715821d722e8aa41830d
Omsurvase24 <omsurvase24@gmail.com>
2025-01-05 20:01:52 +0530

    user service changes

--- app/(dashboard)/u/[username]/(home)/page.tsx
+++ app/(dashboard)/u/[username]/(home)/page.tsx
@@ -1,3 +1,6 @@
+import { getUserByUsername } from "@/lib/user-service";
+import { currentUser } from "@clerk/nextjs/server";
+
 interface CreatorPageProps {
     params: {
         username: string;
@@ -7,6 +10,13 @@ interface CreatorPageProps {
 const CreatorPage = async ({
     params,
 }: CreatorPageProps) => {
+    const externalUser = await currentUser();
+    const user = await getUserByUsername(params.username);
+
+    if (!user || user.externalUserId !== externalUser?.id || !user.stream) {
+        throw new Error("Unauthorized");
+    }
+
     return (
         <div className="h-full">
             Creator Page
