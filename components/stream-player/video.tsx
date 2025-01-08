1e5d9bcbafadc068ee53652f253efbd73e696360
Omsurvase24 <omsurvase24@gmail.com>
2025-01-08 09:44:11 +0530

    video overview

--- components/stream-player/video.tsx
+++ components/stream-player/video.tsx
@@ -1,5 +1,10 @@
 "use client";
 
+import { ConnectionState, Track } from "livekit-client";
+import { useConnectionState, useRemoteParticipant, useTracks, } from "@livekit/components-react";
+import { OfflineVideo } from "./offline-video";
+import { LoadingVideo } from "./loading-video";
+
 interface VideoProps {
     hostName: string;
     hostIdentity: string;
@@ -9,9 +14,26 @@ export const Video = ({
     hostName,
     hostIdentity,
 }: VideoProps) => {
+    const connectionState = useConnectionState();
+    const participant = useRemoteParticipant(hostIdentity);
+    const tracks = useTracks([
+        Track.Source.Camera,
+        Track.Source.Microphone,
+    ]).filter((track) => track.participant.identity === hostIdentity);
+
+    let content;
+
+    if (!participant && connectionState === ConnectionState.Connected) {
+        content = <OfflineVideo username={hostName} />
+    } else if (!participant || tracks.length === 0) {
+        content = <LoadingVideo label={connectionState} />
+    } else {
+        content = <p>Live Video</p>
+    }
+
     return (
-        <div>
-            Video
+        <div className="aspect-video border-b group relative">
+            {content}
         </div>
     );
 };
