f5b8b6612f8eba681cc8640c2f835fae674fe556
Omsurvase24 <omsurvase24@gmail.com>
2025-03-16 04:45:47 +0530

    user page

--- actions/block.ts
+++ actions/block.ts
@@ -1,31 +1,45 @@
-"use server";
+"use server"
 
-import { blockUser, unblockUser } from "@/lib/block-service"
+import { getSelf } from "@/lib/auth-service";
+import { blockUser, unblockUser } from "@/lib/block-service";
+import { RoomServiceClient } from "livekit-server-sdk";
 import { revalidatePath } from "next/cache";
 
+const roomService = new RoomServiceClient(
+    process.env.LIVEKIT_API_URL!,
+    process.env.LIVEKIT_API_KEY!,
+    process.env.LIVEKIT_API_SECRET!,
+)
+
 export const onBlock = async (id: string) => {
-    // TODO: Adapt to disconnect from livestream
-    // TODO: Allow ability to kick the guest
-    const blockedUser = await blockUser(id);
+    const self = await getSelf();
 
-    revalidatePath("/");
+    let blockedUser;
 
-    if (blockedUser) {
-        revalidatePath(`/${blockedUser.blocked.username}`);
+    try {
+        const blockedUser = await blockUser(id);
+    } catch {
+
     }
+
+    try {
+        await roomService.removeParticipant(self.id, id)
+    } catch {
+
+    }
+
+    revalidatePath(`/u/${self.username}/community`);
 
     return blockedUser;
-};
+}
 
 export const onUnblock = async (id: string) => {
+    const self = await getSelf();
     const unblockedUser = await unblockUser(id);
 
-    revalidatePath("/");
 
-    if (unblockedUser) {
-        revalidatePath(`/${unblockedUser.blocked.username}`);
-    }
+
+    revalidatePath(`/u/${self.username}/community`);
 
     return unblockedUser;
-};
-
+}
