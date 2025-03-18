651b5686f5f4de6dd9d769c3ccaa408f6dc28592
Omsurvase24 <omsurvase24@gmail.com>
2025-03-18 23:57:05 +0530

    home page

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,8 +1,12 @@
-// app/page.tsx
+import { Suspense } from "react";
+import { Results, ResultsSkeleton } from "./_components/results";
+
 export default function Page() {
   return (
-    <div className="flex flex-col gap-y-4">
-      <h1>Welcome to the Home Page!</h1>
+    <div className="h-full p-8 max-w-screen-2xl mx-auto">
+      <Suspense fallback={<ResultsSkeleton />}>
+        <Results />
+      </Suspense>
     </div>
   );
 }
